79a25215b5c3bd293a72fcdb93b9a8bcd64bfba6
Rishika Reddy Chennuri <rishikareddychennuri@Rishikas-MacBook-Air.local>
2025-08-01 12:09:31 +0530

    Initial commit with frontend and backend

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# app/main.py
+
+import streamlit as st
+
+st.set_page_config(page_title="AI Health Analyzer", layout="wide")
+
+st.title("🩺 AI Health Report Analyzer")
+st.write("""
+Welcome to your AI-powered health assistant!  
+You can upload blood test reports, CT scans, and other medical documents here.  
+This app will help you analyze them and give you detailed predictions & health dashboards.
+""")
